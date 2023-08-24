53d4333c051d0d6464da81815cf5a004f73b8a59
Enrique Sena <enriquesena2001@gmail.com>
2023-08-23 20:21:27 -0400

    teste

--- Noserver.js
+++ Noserver.js
@@ -4,7 +4,8 @@ const conexaoDb = require ("./Banco/conexaoDb")
 require("dotenv").config();
 require ("./routes/_routesIndex")
 
-
+// hbununuh
+// 6tytby
 const port = process.env.PORTA;
 app.listen(port,() =>{ 
     console.log(`Servidor rodando na porta: ${port}`)
